cb0b481c4f461855079d0c2ae5d84096b750b86e
James Yaguma <jyaguma@gmail.com>
2025-11-22 01:00:53 -0800

    Added Task toJSON and fromJSON methods

--- packages/react-frontend/src/task.jsx
+++ packages/react-frontend/src/task.jsx
@@ -15,10 +15,30 @@ class Task {
     this.description = description;
     this.date = date;
     if (hasTime) this.date?.setSeconds(0);
+    else this.date?.setSeconds(59);
     this.checked = false;
   }
 
+  toJSON() {
+    let result = "{";
+    result += `"title":"${this.title}"`;
+    result += `"_id":"${this._id}"`;
+    result += `"tags":"${this.tags}"`;
+    result += `"description":"${this.description}"`;
+    result += `"date":"${this.date.toJSON()}"`;
+    result += `"checked":"${this.checked}"`;
+    result += "}";
+    return result;
+  }
+
+  static fromJSON(json) {
+    const parsedJSON = JSON.parse(json);
+    const result = new Task(parsedJSON.title, parsedJSON.tags, parsedJSON.description, new Date(parsedJSON.date), parsedJSON.date.slice(17, 19) == "00", parsedJSON._id);
+    result.checked = parsedJSON.checked;
+  }
+
   getData() {
+    console.log(JSON.stringify(this));
     return {
       title: this.title,
       id: this._id,
